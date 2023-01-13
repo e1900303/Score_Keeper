9c91474608ed928978f6d9c39fe45cbfafa1c41d
unknown <tranduyphat080300@gmail.com>
2023-01-13 15:18:35 +0200

    added style

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector('#playTo');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
@@ -15,8 +15,8 @@ p1Button.addEventListener('click', () => {
         p1Score += 1;
         if (p1Score == winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
         }
         p1Display.textContent = p1Score;
     }
@@ -27,8 +27,8 @@ p2Button.addEventListener('click', () => {
         p2Score += 1;
         if (p2Score == winningScore) {
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
         }
         p2Display.textContent = p2Score;
     }
@@ -41,13 +41,13 @@ winningScoreSelect.addEventListener('change', () => {
 
 resetButton.addEventListener('click', reset);
 
-const reset = () => {
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
 }
 
